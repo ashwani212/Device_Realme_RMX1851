ccdf0647180fb13bc91b3f5e4d752154b9606e14
Cykeek <Cykeek@proton.me>
2022-10-25 01:30:24 +0530

    RMX1851: use clang-r437112b
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,9 +19,12 @@ echo "Clonning Vendor..."
 git clone https://github.com/Cykeek-Labs/vendor_realme_RMX1851 -b 13 vendor/realme/RMX1851
 
 # Kernel
+
 banner 2>1
 echo "Cloning Kernel..."
+git clone https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b prebuilts/clang/host/linux-x86/clang-r437112b
 git clone https://github.com/Cykeek-Labs/kernel_realme_sdm710 kernel/realme/sdm710
+
 
 # RealmeParts
 banner 2>1
