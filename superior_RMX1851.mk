c3471a1876f1b77ee2026b16bd7677303832f11f
Noobie_ashwani <99090768+ashwani212@users.noreply.github.com>
2023-01-17 13:16:34 +0530

    Revaluate Previous commit

--- superior_RMX1851.mk
+++ superior_RMX1851.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, device/realme/RMX1851/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/superior/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_RMX1851
